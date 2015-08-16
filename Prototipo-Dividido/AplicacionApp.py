a4039a2b6eedeb01f42bb8032bb04ab393ee04c2
Azalia Jiménez <aza_jimenez@hotmail.com>
2015-08-16 17:16:17 -0500

    Proyecto Final - adicional pantalla de Ingenierias

--- Prototipo-Dividido/AplicacionApp.py
+++ Prototipo-Dividido/AplicacionApp.py
@@ -1,31 +1,26 @@
 #!/usr/bin/env python
 #coding: utf8 
 
-
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import NumericProperty
 from kivy.lang import Builder
 
-
 #Ventanas Principales 
 class Principal(Screen):
     pass
-class Lincenciatura(Screen):
+class Licenciatura(Screen):
     def mostrar(self):
-        print("Lincenciatura- Menu Principal") 
-
-class LincenciaturaPro(Screen):
+        print("Licenciatura- Menu Principal") 
+class LicenciaturaPro(Screen):
     def mostrar(self):
-        print("LincenciaturaPro-Menu Principal")
-
+        print("LicenciaturaPro-Menu Principal")
 class Maestria(Screen):
     def mostrar(self):
         print("Maestria- Menu Principal")
 class Postgrado(Screen):
     def mostrar(self):
         print("Postgrado Menu Principal")
-
 class Diplomado(Screen):
     def mostrar(self):
         print("Diplomado- Menu Principal")
@@ -38,24 +33,43 @@ class Facultades_arquitectura(Screen):
     """Facultades_arquitectura"""
     def mostrar(self):
         print("Facultades-Menu Principal")
+class Facultades_ciencias_salud(Screen):
+    """Facultades_ciencias_salud"""
+    def mostrar(self):
+        print("Facultades-Menu Principal")
+class Facultades_derecho(Screen):
+    """Facultades_derecho"""
+    def mostrar(self):
+        print("Facultades-Menu Principal")
+class Facultades_gastronomia(Screen):
+    """Facultades_gastronomia"""
+    def mostrar(self):
+        print("Facultades-Menu Principal")
+class Facultades_ingenieria(Screen):
+    """Facultades_ingenieria"""
+    def mostrar(self):
+        print("Facultades-Menu Principal")
 class Carreras(Screen):
     def mostrar(self):
         print("Carreras-Menu Principal")
 
 class AplicacionApp(App):
-
     def build(self):
         root = ScreenManager()
         #Pantallas principales
         root.add_widget(Principal(name='principal'))
-        root.add_widget(Lincenciatura(name='lincenciatura'))
-        root.add_widget(LincenciaturaPro(name='lincenciaturaPro'))
+        root.add_widget(Licenciatura(name='licenciatura'))
+        root.add_widget(LicenciaturaPro(name='licenciaturaPro'))
         root.add_widget(Maestria(name='maestria'))
         root.add_widget(Postgrado(name='postgrado'))
         root.add_widget(Diplomado(name='diplomado'))
         #Pantallas sub-Principales
         root.add_widget(Facultades(name='facultades'))
         root.add_widget(Facultades_arquitectura(name='arquitectura'))
+        root.add_widget(Facultades_ciencias_salud(name='salud'))
+        root.add_widget(Facultades_derecho(name='derecho'))
+        root.add_widget(Facultades_gastronomia(name='gastronomia'))
+        root.add_widget(Facultades_ingenieria(name='ingenieria'))
         root.add_widget(Carreras(name='carreras'))
         return root
 

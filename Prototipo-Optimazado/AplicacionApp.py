ea475588d45f9558d9276dd5cf38d48bf55c29a4
maryito <m_torres@uip.edu.pa>
2015-08-21 13:57:15 -0500

    Prototipo optimazado con modo de uso mas facil con etiquetas  lean al pricipio las indicaciones y las funciones que se usan

---
+++ Prototipo-Optimazado/AplicacionApp.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from _CarrerasDB_local  import *
+
+class Ventanas(ScreenManager):
+    nom =""
+    def datos(self,ventana, Resolucion):
+        # Limpiar campos 
+        self.nom = Resolucion
+        self.nombreVentana=ventana
+        print(ventana," ====> ",Resolucion)
+
+    def limpiar(self):
+        self.titulo=""
+        self.resolucion=""
+        self.duracion=""
+        self.sede=""
+        self.horario=""
+        self.modalidad=""
+        print("Limpiando Variables") 
+
+    def carrera(self):
+        print("test: ",self.nom)
+
+        self.busqueda = ""+self.nom
+        print("funcion para : ",self.busqueda )
+
+        # Realizando Consulta y Asignando el resultado de la Consulta
+        valor=DbCarreras(self.busqueda )
+
+        #Asignacion de los Datos Traidos  a la Ventana
+        if self.busqueda  in valor:
+            print(self.busqueda )
+            print("si esta DISPONIBLE")
+            for i in range (len(valor)):
+                if i==0:
+                    print("\t Carrera: ",valor[i])
+                    self.titulo= valor[i]
+                if i==1:
+                    print("\t Resolución: ",valor[i])
+                    self.resolucion= valor[i]
+                if i==2:
+                    print("Duración: ",valor[i])
+                    self.duracion= valor[i]
+                if i==3:
+                    print("Sede: ",valor[i])
+                    self.sede= valor[i]
+                if i==4:
+                    print("Horario: ",valor[i])
+                    self.horario= valor[i]
+                if i==5:
+                    print("Modalidad: ",valor[i])
+                    self.modalidad= valor[i]
+        else:
+            print("no esta DISPONIBLE")
+    
+class AplicacionApp(App):
+	def build(self):
+		return Ventanas()
+
+
+if __name__ == '__main__':
+	AplicacionApp().run()
